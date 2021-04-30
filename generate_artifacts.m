1579f9f9944cb69a84079e1687724d1c46beb756
Austin Hurst <mynameisaustinhurst@gmail.com>
2021-04-30 18:53:26 -0300

    Add extra artifacts for stages of pipeline

--- generate_artifacts.m
+++ generate_artifacts.m
@@ -16,7 +16,7 @@ settings;
 % Load EEGLAB and MATLAB PREP
 addpath(eeglab_path);
 addpath(genpath(matprep_path));
-eeglab nogui;
+eeglab();
 
 % Install BIOSIG package for reading EDF/BDF files
 plugin_askinstall('Biosig', [], true);
@@ -90,6 +90,9 @@ defaults = getPrepDefaults(EEG, 'boundary');
 [boundaryOut, errors] = checkPrepDefaults(params, struct(), defaults);
 EEG.etc.noiseDetection.boundary = boundaryOut;
 
+% Save a copy of the data prior to running PREP
+save_set(EEG, [artifact_dir sep '1_matprep_raw']);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Step 2: Actually run all components of the PREP pipeline %%%
@@ -101,6 +104,7 @@ fprintf('\n\n=== Removing Trends Pre-CleanLine... ===\n\n');
 EEG.etc.noiseDetection.detrend = detrend;
 defaults = getPrepDefaults(EEG, 'detrend');
 params = checkPrepDefaults(detrend, params, defaults);
+save_set(EEGNew, [outdir sep '2_matprep_removetrend']);
 
 % Perform cleanline on data and put trend back
 fprintf('\n\n=== Performing CleanLine... ===\n\n');
@@ -112,10 +116,18 @@ EEG.data(lineChannels, :) = ...
     - EEGNew.data(lineChannels, :) ...
     + EEGClean.data(lineChannels, :);
 clear EEGNew;
+save_set(EEG, [outdir sep '3_matprep_cleanline']);
+
+% Get detrended data prior to rereferencing & save a copy
+fprintf('\n\n=== Get Detrended Signal Pre-Reference... ===\n\n');
+[EEGNew, detrend] = removeTrend(EEG, params);
+save_set(EEGNew, [outdir sep '4_matprep_pre_reference']);
+clear EEGNew;
  
 % Perform robust re-referencing on data
 fprintf('\n\n=== Performing Robust Referencing... ===\n\n');
 [EEG, referenceOut] = performReference(EEG, params);
+save_set(EEG, [outdir sep '5_matprep_post_reference']);
 
 % Save detailed internal PREP details to a .mat
 prep_info = EEG.etc;
@@ -126,6 +138,7 @@ prep_info.noiseDetection.interpolatedChannelNumbers = ...
 prep_info.noiseDetection.stillNoisyChannelNumbers = ...
     referenceOut.noisyStatistics.noisyChannels.all;
 save([artifact_dir sep 'matprep_info'], 'prep_info');
+
 pretty_header([filename ' complete!']);
 
 
@@ -140,4 +153,9 @@ function [] = pretty_header(content);
     fprintf(['\n' header_pad '\n']);
     fprintf([header '\n']);
     fprintf([header_pad '\n\n']);
+end
+
+% Function for saving EEG data to a single .set file
+function [] = save_set(EEG, outpath);
+    pop_saveset(EEG, outpath, 'savemode', 'onefile');
 end
