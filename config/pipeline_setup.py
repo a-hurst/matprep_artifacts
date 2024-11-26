34c2341ed4bfa105a220138d24c433b255c752c4
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2024-11-26 20:26:18 +0100

    run ruff format

--- config/pipeline_setup.py
+++ config/pipeline_setup.py
@@ -11,9 +11,9 @@ from urllib.error import URLError, HTTPError
 
 # Initialize required directories
 
-download_dir = Path('temp')
-package_dir = Path('deps')
-artifact_dir = Path('artifacts')
+download_dir = Path("temp")
+package_dir = Path("deps")
+artifact_dir = Path("artifacts")
 
 download_dir.mkdir(exist_ok=True)
 package_dir.mkdir(exist_ok=True)
@@ -21,15 +21,15 @@ artifact_dir.mkdir(exist_ok=True)
 
 # Download test EEG data (currently using S004R01 from the BCI2000 dataset)
 
-subject = 'S004'
-run = 'R01'
-eeg_filename = '{0}{1}.edf'.format(subject, run)
-eeg_url = 'https://www.physionet.org/files/eegmmidb/1.0.0/{0}/{1}?download'
+subject = "S004"
+run = "R01"
+eeg_filename = "{0}{1}.edf".format(subject, run)
+eeg_url = "https://www.physionet.org/files/eegmmidb/1.0.0/{0}/{1}?download"
 
 print("* Downloading EEG test data...")
 try:
     mod_http = urlopen(eeg_url.format(subject, eeg_filename))
-    with open(eeg_filename, 'wb') as out:
+    with open(eeg_filename, "wb") as out:
         out.write(mod_http.read())
 except (URLError, HTTPError):
     raise RuntimeError("Failed to download '{0}'".format(eeg_filename))
@@ -37,26 +37,26 @@ except (URLError, HTTPError):
 # Download and extract EEGLAB and MATLAB PREP
 
 pkgs = {
-    'EEGLAB': 'https://sccn.ucsd.edu/eeglab/currentversion/eeglab2021.0.zip',
-    'PREP': 'https://github.com/VisLab/EEG-Clean-Tools/archive/refs/heads/master.zip'
+    "EEGLAB": "https://sccn.ucsd.edu/eeglab/currentversion/eeglab2021.0.zip",
+    "PREP": "https://github.com/VisLab/EEG-Clean-Tools/archive/refs/heads/master.zip",
 }
 
 for name, url in pkgs.items():
     print("* Downloading {0}...".format(name))
 
     # Download .zip to temporary folder
-    download_path = download_dir / f'{name}.zip'
+    download_path = download_dir / f"{name}.zip"
     try:
         mod_http = urlopen(url)
-        with open(download_path, 'wb') as out:
+        with open(download_path, "wb") as out:
             out.write(mod_http.read())
     except (URLError, HTTPError):
         raise RuntimeError("Failed to download '{0}'".format(url))
 
     # Unzip downloaded file to MATLAB package directory
-    with ZipFile(download_path, 'r') as z:
+    with ZipFile(download_path, "r") as z:
         z.extractall(package_dir)
-        root_dir = z.namelist()[0].split('/')[0]
+        root_dir = z.namelist()[0].split("/")[0]
         outpath = package_dir / root_dir
 
     # Rename unzipped package folder to package name for consistency
