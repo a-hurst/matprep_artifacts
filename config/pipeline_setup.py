c7e99e5329afc505ba071f856242f47373fe87a7
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2024-12-05 18:44:04 +0100

    Update workflow, add MIT License (#2)
    * add MIT license, pyprep devs, 2021
    
    * replace os.path with pathlib
    
    * run ruff format
    
    * update github workflow
    
    * use current EEGLAB
    
    * add signal processing toolbox for matlab setup
    
    * add Statistics_and_Machine_Learning_Toolbox to matlab setup
    
    * also run once a month on CRON schedule
    
    * pin EEGLAB version to 2024.2

--- config/pipeline_setup.py
+++ config/pipeline_setup.py
@@ -1,67 +1,65 @@
-### Helper script for setting up MATLAB PREP CI environment ###
+"""Set up MATLAB PREP CI environment."""
 
-# Author: Austin Hurst
+# Authors: Austin Hurst
+#          Stefan Appelhoff
 
-import os
+from pathlib import Path
 import shutil
 from zipfile import ZipFile
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 
-
 # Initialize required directories
 
-download_dir = 'temp'
-package_dir = 'deps'
-artifact_dir = 'artifacts'
+download_dir = Path("temp")
+package_dir = Path("deps")
+artifact_dir = Path("artifacts")
 
-os.mkdir(download_dir)
-os.mkdir(package_dir)
-os.mkdir(artifact_dir)
-
+download_dir.mkdir(exist_ok=True)
+package_dir.mkdir(exist_ok=True)
+artifact_dir.mkdir(exist_ok=True)
 
 # Download test EEG data (currently using S004R01 from the BCI2000 dataset)
 
-subject = 'S004'
-run = 'R01'
-eeg_filename = '{0}{1}.edf'.format(subject, run)
-eeg_url = 'https://www.physionet.org/files/eegmmidb/1.0.0/{0}/{1}?download'
+subject = "S004"
+run = "R01"
+eeg_filename = "{0}{1}.edf".format(subject, run)
+eeg_url = "https://www.physionet.org/files/eegmmidb/1.0.0/{0}/{1}?download"
 
 print("* Downloading EEG test data...")
 try:
     mod_http = urlopen(eeg_url.format(subject, eeg_filename))
-    with open(eeg_filename, 'wb') as out:
+    with open(eeg_filename, "wb") as out:
         out.write(mod_http.read())
 except (URLError, HTTPError):
     raise RuntimeError("Failed to download '{0}'".format(eeg_filename))
 
-
-
 # Download and extract EEGLAB and MATLAB PREP
-
+# for current EEGLAB, see: https://sccn.ucsd.edu/eeglab/currentversion/eeglab_current.zip
+# for other EEGLAB versions, see: https://sccn.ucsd.edu/eeglab/download/daily/
 pkgs = {
-    'EEGLAB': 'https://sccn.ucsd.edu/eeglab/currentversion/eeglab2021.0.zip',
-    'PREP': 'https://github.com/VisLab/EEG-Clean-Tools/archive/refs/heads/master.zip'
+    "EEGLAB": "https://sccn.ucsd.edu/eeglab/download/daily/eeglab2024.2.zip",
+    "PREP": "https://github.com/VisLab/EEG-Clean-Tools/archive/refs/heads/master.zip",
 }
 
 for name, url in pkgs.items():
     print("* Downloading {0}...".format(name))
 
     # Download .zip to temporary folder
-    download_path = os.path.join(download_dir, '{0}.zip'.format(name))
+    download_path = download_dir / f"{name}.zip"
     try:
         mod_http = urlopen(url)
-        with open(download_path, 'wb') as out:
+        with open(download_path, "wb") as out:
             out.write(mod_http.read())
     except (URLError, HTTPError):
         raise RuntimeError("Failed to download '{0}'".format(url))
 
     # Unzip downloaded file to MATLAB package directory
-    with ZipFile(download_path, 'r') as z:
+    with ZipFile(download_path, "r") as z:
         z.extractall(package_dir)
-        root_dir = z.namelist()[0].split('/')[0]
-        outpath = os.path.join(package_dir, root_dir)
+        root_dir = z.namelist()[0].split("/")[0]
+        outpath = package_dir / root_dir
 
     # Rename unzipped package folder to package name for consistency
-    pkg_path = os.path.join(package_dir, name)
+    pkg_path = package_dir / name
     shutil.move(outpath, pkg_path)
